87a3ada04408bde2adabe008655c76168bdd950f
Ben Marx <bmarx@bleacherreport.com>
2017-08-20 22:16:57 -0400

    add function problem

---
+++ functions/multiple_function_defs.exs
@@ -0,0 +1,8 @@
+defmodule MultipleFunctionDefs do
+  @moduledoc """
+  Given one argument, an integer, if the integer is less than 0 return :error,
+  if it's between 1 and 10 return :ok and if it's greater than 10 return :great. What happens if an integer
+  isn't passed in as an argument? Do you want to handle that case or not? If so, how?
+  """
+  def widget_calculator
+end
